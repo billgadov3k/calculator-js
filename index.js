2d4f4d28afa01a3fb15cf98b32026160847f37cc
Aleksandr Tugolukov <billgadov3k@gmail.com>
2022-04-28 20:55:34 +0300

    add code after refactoring

--- index.js
+++ index.js
@@ -1,51 +1,43 @@
-let btnC = document.querySelector(".btn-с");
+let queryResult = document.querySelector(".result");
 
-btnC.addEventListener("click", () => {
-  document.querySelector(".result").innerHTML = "";
+document.querySelector(".btnC").addEventListener("click", () => {
+  queryResult.innerHTML = "";
 });
 
-let btnOne = document.querySelector(".btn-one"); // 1
-let btnTwo = document.querySelector(".btn-two"); // 2
-let btnThree = document.querySelector(".btn-three"); // 3
-let btnFour = document.querySelector(".btn-four"); // 4
-let btnFive = document.querySelector(".btn-five"); // 5
-let btnSix = document.querySelector(".btn-six"); // 6
-let btnSeven = document.querySelector(".btn-seven"); // 7
-let btnEight = document.querySelector(".btn-eight"); // 8
-let btnNine = document.querySelector(".btn-nine"); // 9
-let btnZero = document.querySelector(".btn-zero"); // 0
-let btnPlus = document.querySelector(".btn-plus");
-let btnMinus = document.querySelector(".btn-minus");
-let btnMult = document.querySelector(".btn-multiply");
-let btnDivis = document.querySelector(".btn-divis");
-let btnResult = document.querySelector(".btn-result");
+const buttons = [
+  {selector: "btnOne", param: 1},
+  {selector: "btnTwo", param: 2},
+  {selector: "btnThree", param: 3},
+  {selector: "btnMultiply", param: "*"},
+  {selector: "btnFour", param: 4},
+  {selector: "btnFive", param: 5},
+  {selector: "btnSix", param: 6},
+  {selector: "btnDivis", param: "/"},
+  {selector: "btnSeven", param: 7},
+  {selector: "btnEight", param: 8},
+  {selector: "btnNine", param: 9},
+  {selector: "btnPlus", param: "+"},
+  {selector: "btnZero", param: 0},
+  {selector: "btnResult", param: -1},
+  {selector: "btnMinus", param: "-"},
+];
 
-btnResult.addEventListener("click", () => addResul());
-
-btnOne.addEventListener("click", () => check(1));
-btnTwo.addEventListener("click", () => check(2));
-btnThree.addEventListener("click", () => check(3));
-btnFour.addEventListener("click", () => check(4));
-btnFive.addEventListener("click", () => check(5));
-btnSix.addEventListener("click", () => check(6));
-btnSeven.addEventListener("click", () => check(7));
-btnEight.addEventListener("click", () => check(8));
-btnNine.addEventListener("click", () => check(9));
-btnZero.addEventListener("click", () => check(0));
-
-btnPlus.addEventListener("click", () => check("+"));
-btnMinus.addEventListener("click", () => check("-"));
-btnMult.addEventListener("click", () => check("*"));
-btnDivis.addEventListener("click", () => check("/"));
+buttons.forEach((value) => {
+    let el = document.querySelector(`.${value.selector}`);
+    if (value.param == -1){
+      el.addEventListener("click", () => addResul());
+    } else {
+      el.addEventListener("click", () => check(value.param));
+    }
+} );
 
 function check(one) {
-  document.querySelector(".result").innerHTML =
-    document.querySelector(".result").innerHTML + one;
+  queryResult.innerHTML = queryResult.innerHTML + one;
 }
 
 function addResul() {
-  let result = eval(document.querySelector(".result").innerHTML);
+  let result = eval(queryResult.innerHTML);
   if (result) {
-    document.querySelector(".result").innerHTML = window.eval(result);
+    queryResult.innerHTML = window.eval(result);
   }
 }
